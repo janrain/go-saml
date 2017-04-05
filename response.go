3479374ae5cc5ca892a2622a6bcdffc16392042b
Eric Beeson <ebeeson@janrain.com>
2017-04-05 16:46:14 -0700

    CIA-272 add NewResponse and AddAttribute back

--- response.go
+++ response.go
@@ -92,3 +92,180 @@ func (sp *ServiceProvider) ValidateResponse(resp *Response) error {
 
 	return nil
 }
+
+func NewResponse() *Response {
+	return &Response{
+		XMLName: xml.Name{
+			Local: "samlp:Response",
+		},
+		SAMLP:        "urn:oasis:names:tc:SAML:2.0:protocol",
+		SAML:         "urn:oasis:names:tc:SAML:2.0:assertion",
+		SAMLSIG:      "http://www.w3.org/2000/09/xmldsig#",
+		ID:           util.ID(),
+		Version:      "2.0",
+		IssueInstant: time.Now().UTC().Format(time.RFC3339Nano),
+		Issuer: Issuer{
+			XMLName: xml.Name{
+				Local: "saml:Issuer",
+			},
+			Url: "", // caller must populate ar.AppSettings.AssertionConsumerServiceURL,
+		},
+		Signature: Signature{
+			XMLName: xml.Name{
+				Local: "samlsig:Signature",
+			},
+			Id: "Signature1",
+			SignedInfo: SignedInfo{
+				XMLName: xml.Name{
+					Local: "samlsig:SignedInfo",
+				},
+				CanonicalizationMethod: CanonicalizationMethod{
+					XMLName: xml.Name{
+						Local: "samlsig:CanonicalizationMethod",
+					},
+					Algorithm: "http://www.w3.org/2001/10/xml-exc-c14n#",
+				},
+				SignatureMethod: SignatureMethod{
+					XMLName: xml.Name{
+						Local: "samlsig:SignatureMethod",
+					},
+					Algorithm: "http://www.w3.org/2000/09/xmldsig#rsa-sha1",
+				},
+				SamlsigReference: SamlsigReference{
+					XMLName: xml.Name{
+						Local: "samlsig:Reference",
+					},
+					URI: "", // caller must populate "#" + ar.Id,
+					Transforms: Transforms{
+						XMLName: xml.Name{
+							Local: "samlsig:Transforms",
+						},
+						Transform: Transform{
+							XMLName: xml.Name{
+								Local: "samlsig:Transform",
+							},
+							Algorithm: "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
+						},
+					},
+					DigestMethod: DigestMethod{
+						XMLName: xml.Name{
+							Local: "samlsig:DigestMethod",
+						},
+						Algorithm: "http://www.w3.org/2000/09/xmldsig#sha1",
+					},
+					DigestValue: DigestValue{
+						XMLName: xml.Name{
+							Local: "samlsig:DigestValue",
+						},
+					},
+				},
+			},
+			SignatureValue: SignatureValue{
+				XMLName: xml.Name{
+					Local: "samlsig:SignatureValue",
+				},
+			},
+			KeyInfo: KeyInfo{
+				XMLName: xml.Name{
+					Local: "samlsig:KeyInfo",
+				},
+				X509Data: X509Data{
+					XMLName: xml.Name{
+						Local: "samlsig:X509Data",
+					},
+					X509Certificate: X509Certificate{
+						XMLName: xml.Name{
+							Local: "samlsig:X509Certificate",
+						},
+						Cert: "", // caller must populate cert,
+					},
+				},
+			},
+		},
+		Status: Status{
+			XMLName: xml.Name{
+				Local: "samlp:Status",
+			},
+			StatusCode: StatusCode{
+				XMLName: xml.Name{
+					Local: "samlp:StatusCode",
+				},
+				// TODO unsuccesful responses??
+				Value: "urn:oasis:names:tc:SAML:2.0:status:Success",
+			},
+		},
+		Assertion: Assertion{
+			XMLName: xml.Name{
+				Local: "saml:Assertion",
+			},
+			XS:           "http://www.w3.org/2001/XMLSchema",
+			XSI:          "http://www.w3.org/2001/XMLSchema-instance",
+			SAML:         "urn:oasis:names:tc:SAML:2.0:assertion",
+			Version:      "2.0",
+			ID:           util.ID(),
+			IssueInstant: time.Now().UTC().Format(time.RFC3339Nano),
+			Issuer: Issuer{
+				XMLName: xml.Name{
+					Local: "saml:Issuer",
+				},
+				Url: "", // caller must populate ar.AppSettings.AssertionConsumerServiceURL,
+			},
+			Subject: Subject{
+				XMLName: xml.Name{
+					Local: "saml:Subject",
+				},
+				NameID: NameID{
+					XMLName: xml.Name{
+						Local: "saml:NameID",
+					},
+					Format: "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
+					Value:  "",
+				},
+				SubjectConfirmation: SubjectConfirmation{
+					XMLName: xml.Name{
+						Local: "saml:SubjectConfirmation",
+					},
+					Method: "urn:oasis:names:tc:SAML:2.0:cm:bearer",
+					SubjectConfirmationData: SubjectConfirmationData{
+						InResponseTo: "",
+						NotOnOrAfter: time.Now().Add(time.Minute * 5).UTC().Format(time.RFC3339Nano),
+						Recipient:    "",
+					},
+				},
+			},
+			Conditions: Conditions{
+				XMLName: xml.Name{
+					Local: "saml:Conditions",
+				},
+				NotBefore:    time.Now().Add(time.Minute * -5).UTC().Format(time.RFC3339Nano),
+				NotOnOrAfter: time.Now().Add(time.Minute * 5).UTC().Format(time.RFC3339Nano),
+			},
+			AttributeStatement: AttributeStatement{
+				XMLName: xml.Name{
+					Local: "saml:AttributeStatement",
+				},
+				Attributes: []Attribute{},
+			},
+		},
+	}
+}
+
+// AddAttribute add attribute to the Response
+func (r *Response) AddAttribute(name, value string) {
+	r.Assertion.AttributeStatement.Attributes = append(r.Assertion.AttributeStatement.Attributes, Attribute{
+		XMLName: xml.Name{
+			Local: "saml:Attribute",
+		},
+		Name:       name,
+		NameFormat: "urn:oasis:names:tc:SAML:2.0:attrname-format:basic",
+		AttributeValues: []AttributeValue{
+			{
+				XMLName: xml.Name{
+					Local: "saml:AttributeValue",
+				},
+				Type:  "xs:string",
+				Value: value,
+			},
+		},
+	})
+}
